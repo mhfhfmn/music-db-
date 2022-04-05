6df4d7534bb4493dd29dfd365734bf8368cf29ce
MHFhfmn <kyrsivom@gmail.com>
2022-04-05 18:33:40 +0300

    punkt 1 ready

--- Script-2.sql
+++ Script-2.sql
@@ -1,3 +1,10 @@
 delete from genresartist;
+delete from artistalbum;
+delete from compilationtrack;
+delete from track;
 delete from genre;
 delete from artist;
+delete from album;
+delete from compilation 
+
+
